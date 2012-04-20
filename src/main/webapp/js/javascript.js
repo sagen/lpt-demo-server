25c63ade28e4ee95729c984bf7ceb67260e5d4c8
Anders Kristiansen <anderskri@gmail.com>
2012-04-20 17:28:17 +0200

    added a swipe to delete plugin we might need

---
+++ src/main/webapp/js/javascript.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	
+//	$('#page2').live('pageshow',function(event, ui){
+//        populateList(list); //TODO: implement function
+//    })
+	
+//	$('list').listview(); //Might need to restyle list after init
+	
+	$('#list li').swipeDelete({
+	    click: function(e){
+	    	// This should also update backend
+	        e.preventDefault();
+	        $(this).parents('li').remove();
+	    }
+	});
+
+	
+});
+
