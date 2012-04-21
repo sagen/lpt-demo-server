925f6b8bc85c45baa6f1e9558c5474da949813b7
Anders Kristiansen <anderskri@gmail.com>
2012-04-22 01:22:39 +0200

    Not totally happy with the code, but it is a start.

--- src/main/webapp/js/setup.js
+++ src/main/webapp/js/setup.js
@@ -1,8 +1,9 @@
 $(document).bind("mobileinit", function(){
 	$.extend($.mobile,{
-		defaultPageTransition: 'slidefade', //or slide?
+		defaultPageTransition: 'fade', //or slide?
 		defaultDialogTransition: 'pop',
 	  });
+	
 	
 	$.mobile.page.prototype.options.addBackBtn = true;
 	$.mobile.page.prototype.options.backBtnText = "Tilbake";
@@ -18,19 +19,35 @@ $(document).bind("mobileinit", function(){
     $.mobile.listview.prototype.options.filterTheme = "b";
     
     
+    
 	 // Listen for any attempts to call changePage().
      // This one checks if the requested page is a company page,
      // and fires a call to getCompany with the specified id in the URL
     $(document).bind( "pagebeforechange", function( e, data ) {
-    	if($(data.toPage)[0]==$('#welcome-page')[0]){
-    		populateCompanies($('#company-list'));
+    	if ( typeof data.toPage === "string" ) {
+
+    		var u = $.mobile.path.parseUrl( data.toPage );
+
+    		if ( u.hash.search(/^#agreement-page/) !== -1 ) {
+    			$.mobile.showPageLoadingMsg();
+    			e.preventDefault();
+    			populateAgreementPage( u, data.options , $('#agreement-page'));
+    			
+    		} else if ( u.hash.search(/^#agreement-list-page/) !== -1 ) {
+    			$.mobile.showPageLoadingMsg();
+    			e.preventDefault();
+    			populateAgreements( u, data , $('#agreement-list-page') , $('#agreement-list'));
+    		} 
+
+    	}else if($(data.toPage)[0]==$('#welcome-page')[0]){
+    		populateCompanies( u, data.options , $('#welcome-page'), $('#company-list'));
     	}
 
     });
     
-	function populateCompanies(list){
+    
+	function populateCompanies( u, data , page, list){
 		list.empty();
-		$.mobile.showPageLoadingMsg();		
 		$.ajax({
 			url: '/rest/companies/',
 			dataType: "json",
@@ -40,96 +57,121 @@ $(document).bind("mobileinit", function(){
 					var id = data[i].id;
 					var name = data[i].name;
 					list.append('<li data-companyid="'+id+'" data-companyname="'+name+'" data-swipeurl="rest/companies/'+id+'">' +
-							'<a href="#">'+name+'</a></li>');
+							'<a href="#agreement-list-page?companyid='+id+'">'+name+'</a></li>');
 				    }  
 
-				list.listview('refresh'); //Might need to restyle list after init
+				page.page();
 				
-				$.mobile.hidePageLoadingMsg();
+				list.listview('refresh'); //Might need to restyle list after init
 			}
 		});
 	}
 
-//    
-//	// Load the data for a specific agreement, based on
-//	// the URL passed in. 
-//	function getAgreement( urlObj, options )
-//	{
-//		var agreementId = urlObj.hash.replace( /.*agreementid=/, "" );
-//
-//			// Get the object that represents the category we
-//			// are interested in. Note, that at this point we could
-//			// instead fire off an ajax request to fetch the data, but
-//			// for the purposes of this sample, it's already in memory.
-//		var agreement = {};
-//		$.mobile.showPageLoadingMsg();
-//		$.ajax({
-//			url: '/rest/companies//agreements/'+agreementId,
-//			dataType: "json",
-//			success : function(data){
-//				var pageSelector = urlObj.hash.replace( /\?.*$/, "" );
-//				populateAgreementPage(data, pageSelector, options, urlObj);
-//				$.mobile.hidePageLoadingMsg();
-//			}
-//		});
-//	}
-//	
-//	// Generate markup for the agreement,
-//	// inject it into an embedded page, and then make
-//	// that page the current active page.
-//	function populateAgreementPage(agreement, pageSelector, options, urlObj)
-//	{
-//			// The pages we use to display our content are already in
-//			// the DOM. The id of the page we are going to write our
-//			// content into is specified in the hash before the '?'.
-//		if ( agreement ) {
-//			// Get the page we are going to dump our content into.
-//			var $page = $( pageSelector ),
-//				// Get the content area element for the page.
-//				$content = $page.children( ":jqmData(role=content)" ),
-//
-//				// The markup we are going to inject into the content
-//				// area of the page.
-//				markup = "<p>Medlemsoversikt for " + agreement.navn + 
-//				"</p><ul data-role='listview' data-inset='true'>",
-//
-//				// The array of items for this category.
-//				members = agreement.medlemmer,
-//
-//				// The number of items in the category.
-//				numMembers = members.length;
-//
-//			// Generate a list item for each item in the category
-//			// and add it to our markup.
-//			for ( var i = 0; i < numMembers; i++ ) {
-//				markup += '<li><a href="#">' + members[i].navn + '</a></li>';
-//			}
-//			markup += "</ul>";
-//
-//
-//			// Inject the company markup into the content element.
-//			$content.html( markup );
-//
-//			// Pages are lazily enhanced. We call page() on the page
-//			// element to make sure it is always enhanced before we
-//			// attempt to enhance the markup we just injected.
-//			// Subsequent calls to page() are ignored since a page/widget
-//			// can only be enhanced once.
-//			$page.page();
-//			
-//			// Enhance the listview we just injected.
-//			$content.find( ":jqmData(role=listview)" ).listview();
-//
-//			// We don't want the data-url of the page we just modified
-//			// to be the url that shows up in the browser's location field,
-//			// so set the dataUrl option to the URL for the category
-//			// we just loaded.
-//			options.dataUrl = urlObj.href;
-//
-//			// Now call changePage() and tell it to switch to
-//			// the page we just modified.
-//			$.mobile.changePage( $page, options );
+	function populateAgreements( urlObj, options , page, list){
+	var companyId = getURLParameter("companyid", urlObj.href);
+	
+	$.ajax({
+		url: '/rest/companies/'+companyId+'/agreements',
+		dataType: "json",
+		success : function(data){
+			list.empty();''
+			markup = "";
+			
+			for(var i = 0; i<data.length; i++)
+			{
+				markup += ('<li data-agreementid="'+data[i].id+'"'+
+						'data-agreementname="'+data[i].name+'"'+
+						'data-companyid="'+companyId+'"'+
+						'data-swipeurl="rest/companies/'+companyId+'/agreements/'+data[i].id+'">'+
+						'<a href="#agreement-page?agreementid='+data[i].id+'">'+data[i].name+'</a></li>');
+			}  
+
+			list.append(markup);
+
+			page.page();
+
+
+			list.listview('refresh'); 
+
+			attachSwipeDeleteListener();
+
+			options.dataUrl = urlObj.href;
+
+			$.mobile.changePage(page, options);
+			$.mobile.hidePageLoadingMsg();
+		}
+	});
+}
+    
+	
+	function populateAgreementPage(urlObj , options , page){
+		var agreementId = getURLParameter("agreementid", urlObj.href);
+		var companyId = getURLParameter("companyid", urlObj.href);
+		$content = $(page).children( ":jqmData(role=content)" ),
+
+		// The markup we are going to inject into the content
+		// area of the page.
+		markup = "<p>Medlemsoversikt for " + agreementId + 
+		"</p><ul data-role='listview' data-inset='true'>",
+
+		// The array of items for this agreement.
+//		members = agreement.medlemmer,
+
+//		// The number of items in the category.
+//		numMembers = members.length;
+
+//		// Generate a list item for each item in the category
+//		// and add it to our markup.
+//		for ( var i = 0; i < numMembers; i++ ) {
+//		markup += '<li><a href="#">' + members[i].navn + '</a></li>';
 //		}
-//	}
+
+		markup += "</ul>";
+
+
+		// Inject the company markup into the content element.
+		$content.html( markup );
+
+		// Pages are lazily enhanced. We call page() on the page
+		// element to make sure it is always enhanced before we
+		// attempt to enhance the markup we just injected.
+		// Subsequent calls to page() are ignored since a page/widget
+
+
+		// We don't want the data-url of the page we just modified
+		// to be the url that shows up in the browser's location field,
+		// so set the dataUrl option to the URL for the category
+		// we just loaded.
+		options.dataUrl = urlObj.href;
+
+		$.mobile.changePage(page, options);
+		$.mobile.hidePageLoadingMsg();
+	}
+	
+	function getURLParameter(name, hash) {
+	    return decodeURI(
+	        (RegExp('.*'+name + '=' + '(.+?)(&|$)').exec(hash)||[,null])[1]
+	    );
+	}
+	
+	function attachSwipeDeleteListener(){
+		$('#agreement-list li').swipeDelete({
+			btnLabel: 'Slett',
+			btnClass: 'aSwipeButton',
+			click: function(e){
+				e.preventDefault();
+				e.stopPropagation();
+				var url = $(e.target).closest('a.aSwipeButton').attr('href');
+				console.log($(e.target));
+				$(this).parents('li').slideUp();
+				$.ajax({
+					url: url, 
+					type: 'POST',
+					success:function(data) {
+						return false;
+					}});
+			}
+		});
+	}
     
 });
