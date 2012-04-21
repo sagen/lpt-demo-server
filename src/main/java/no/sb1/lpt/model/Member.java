73c4ab196da1b75d6011aeefe6728f242841f297
Anders Kristiansen <anderskri@gmail.com>
2012-04-21 17:32:39 +0200

    Added some data to work with

---
+++ src/main/java/no/sb1/lpt/model/Member.java
@@ -0,0 +1,18 @@
+package no.sb1.lpt.model;
+
+import java.util.Date;
+
+public class Member {
+
+	public String navn;
+	public String fnr;
+	public String status;
+	public double aarslonn;
+	public double pensjonsgrunnlag;
+	public int stillingsprosent;
+	public boolean fulltArbeidsdyktig;
+	public Date endret;
+	public boolean laast;
+
+	
+}
