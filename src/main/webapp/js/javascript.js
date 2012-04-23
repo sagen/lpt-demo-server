65f48977d9723b9afdbdae2e4954ded82d325a87
Anders Kristiansen <anderskri@gmail.com>
2012-04-23 17:44:35 +0200

    geolocation example

--- src/main/webapp/js/javascript.js
+++ src/main/webapp/js/javascript.js
@@ -5,5 +5,20 @@ $(document).ready(function(){
         $(this).find('.ui-listview').listview('refresh');
     });
     
+    if (navigator.geolocation) {
+	  navigator.geolocation.getCurrentPosition(success, error);
+	} else {
+	  error('not supported');
+	}
+
+    function success(position)
+    {
+    	alert("found you at "+position.coords.latitude + " " + position.coords.longitude);
+    }
+    
+    function error(msg)
+    {
+    	alert("didnt find you "+msg);
+    }
     
 });
