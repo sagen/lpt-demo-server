339706217c0c3c06fc18bf832a2e4cf966dfea3e
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2012-04-24 15:10:24 +0200

    moved javascript.js to framework.js

--- src/main/webapp/js/framework.js
+++ src/main/webapp/js/framework.js
@@ -127,3 +127,9 @@ function getDateAsString(milliseconds){
 	return time;
 }
 
+$(document).bind("mobileinit", function() {
+    $('[data-role=page]').bind('pageshow', function () {
+        //refresh the listview on the current page (if it exists)
+        $(this).find('.ui-listview').listview('refresh');
+    });
+});
--- src/main/webapp/js/javascript.js
+++
@@ -1,6 +0,0 @@
-$(document).bind("mobileinit", function(){
-    $('[data-role=page]').bind('pageshow', function () {
-        //refresh the listview on the current page (if it exists)
-        $(this).find('.ui-listview').listview('refresh');
-    });
-});
