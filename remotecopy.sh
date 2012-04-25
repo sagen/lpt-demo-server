e71787231f632e469bdf0af616d5ed68251ebf76
Torstein Gjengedal <torstein.gjengedal@gmail.com>
2012-04-25 13:11:21 +0200

    Fixed bug and changed to rsync

--- remotecopy.sh
+++ remotecopy.sh
@@ -1,3 +1,2 @@
-!/bin/bash
-kodepath=kode
-while true; do tmp=`find $kodepath -type f -exec cat {} + | shasum`; if [ "$previoussha" != "$tmp" ]; then echo; echo `date` Change detected! Copying to remote server; echo; scp -r $kodepath/* $lptgrp@lpt.cookiejar.no:web/; previoussha=$tmp; fi; sleep 1; done;
+#!/bin/bash
+while true; do rsync -avz kode/* $lptgrp@lpt.cookiejar.no:web/; sleep 5; done;
